969ca6910bb312d41bd06741b41561625332fba1
Abigail-Cobbina <a.amaopooco@alustudent.com>
2026-07-17 02:34:17 -0700

    Repository setup and adding the .sql file

---
+++ alu_dbase.sql
@@ -0,0 +1,52 @@
+-- =========================================================
+-- ALU Database Project - Group 12
+-- Database: alu_dbase
+-- =========================================================
+-- NORMALIZATION PARAGRAPH (to be filled in by the group once
+-- all tables and data exist)
+-- =========================================================
+
+
+-- =========================================================
+-- DATABASE SETUP
+-- =========================================================
+CREATE DATABASE alu_dbase;
+USE alu_dbase;
+
+
+-- =========================================================
+-- CREATE TABLE STATEMENTS (in dependency order)
+-- =========================================================
+
+-- Wezzie: Classroom
+-- (Wezzie's CREATE TABLE goes here)
+
+-- Abigail: Faculty
+-- (Abigail's CREATE TABLE goes here)
+
+-- Arnold: Students
+-- (Arnold's CREATE TABLE goes here)
+
+-- Acquah: Courses
+-- (Acquah's CREATE TABLE goes here)
+
+-- Yom: Extra_Curricular_Activities
+-- (Yom's CREATE TABLE goes here)
+
+-- Michael: Student_Courses, Student_Activities (junction tables)
+-- (Michael's CREATE TABLE statements go here)
+
+
+-- =========================================================
+-- INSERT STATEMENTS
+-- =========================================================
+
+
+-- =========================================================
+-- INDIVIDUAL UPDATE / DELETE / SELECT (labeled by member)
+-- =========================================================
+
+
+-- =========================================================
+-- GROUP TASKS: JOIN QUERIES + AGGREGATE QUERY
+-- =========================================================
